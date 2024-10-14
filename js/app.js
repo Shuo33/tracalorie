7bff19dd942cd0ebd981e9a556d2dbc21f0539ad
shuo xu <shuo.xu@2021.icam.fr>
2024-10-14 12:06:17 +0200

    display tracker calories numbers

--- js/app.js
+++ js/app.js
@@ -1,19 +1,77 @@
 class CalorieTracker {
+    // constructor runs when app loads 
     constructor() {
-        this._calorieLimit = 2000; 
+        this._calorieLimit = 2500; 
         this._totalCalories = 0; 
         this._meals = [];
         this._workouts = [];
+
+        // the constructor runs immediately when you instantiate the class
+        this._displayCaloriesLimit();
+        this._displayCaloriesTotal();
+        this._displayCaloriesConsumed();
+        this._displayCaloriesBurned();
+        this._displayCaloriesRemaining();
     }
 
+    // Public Methods / API
     addMeal(meal) {
         this._meals.push(meal);
         this._totalCalories += meal.calories; 
+        // after every changes, we need to updated it to the DOM
+        this._render();
     }
 
     addWorkout(workout) {
         this._workouts.push(workout);
         this._totalCalories -= workout.calories;
+        //after every changes, we need to updated it to the DOM so to show it
+        this._render(); 
+    }
+
+    // Private Methods
+    _displayCaloriesTotal() {
+        const totalCaloriesEl = document.getElementById('calories-total');
+        totalCaloriesEl.innerHTML = this._totalCalories; 
+    }
+
+    _displayCaloriesLimit() {
+        const caloriesLimitEl = document.getElementById('calories-limit');
+        caloriesLimitEl.innerHTML = this._calorieLimit; 
+    }
+
+    _displayCaloriesConsumed() {
+        const caloriesConsumedEl = document.getElementById('calories-consumed');
+
+        const consumed = this._meals.reduce(function (total, meal) {
+            return total + meal.calories;
+        }, 0);
+
+        caloriesConsumedEl.innerHTML = consumed; 
+    }
+
+    _displayCaloriesBurned() {
+        const caloriesBurnedEl = document.getElementById('calories-burned');
+        const burned = this._workouts.reduce(function (total, workout) {
+            return total + workout.calories;
+        }, 0);
+        
+        caloriesBurnedEl.innerHTML = burned; 
+    }
+
+    _displayCaloriesRemaining() {
+        const caloriesRemainingEl = document.getElementById('calories-remaining');
+        const remaining = this._calorieLimit - this._totalCalories;
+        caloriesRemainingEl.innerHTML = remaining;
+    }
+
+
+    // after every changement, we need to rend it so the systeme can update it 
+    _render() {
+        this._displayCaloriesTotal();
+        this._displayCaloriesConsumed();
+        this._displayCaloriesBurned();
+        this._displayCaloriesRemaining();
     }
 }
 
